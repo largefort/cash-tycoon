07278afaf6f280c4df73fa8ead9355f8b3fdc881
jafetegill17 <106451813+largefort@users.noreply.github.com>
2023-11-26 17:40:04 +0000

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,11 @@ let clickValue = 1;
 let cps = parseFloat(localStorage.getItem('cps')) || 0;
 let upgrades = parseInt(localStorage.getItem('upgrades')) || 0;
 let upgradeCost = 10;
-let lastUpdateTime = new Date().getTime(); // Initialize last update time
 
 function updateDisplay() {
     document.getElementById('cash').textContent = cash.toFixed(2);
     document.getElementById('cps').textContent = cps.toFixed(2);
     document.getElementById('upgrades').textContent = upgrades;
-    document.getElementById('upgradeCost').textContent = upgradeCost.toFixed(2);
 }
 
 function clickCash() {
@@ -23,7 +21,7 @@ function purchaseUpgrade() {
         cash -= upgradeCost;
         upgrades++;
         cps += 0.1; // Increase cash per second by 0.1 for each upgrade
-        upgradeCost *= 1.5; // Increase the upgrade cost for the next upgrade
+        upgradeCost *= 2; // Double the upgrade cost for the next upgrade
         updateDisplay();
         saveGameState();
     } else {
@@ -32,11 +30,7 @@ function purchaseUpgrade() {
 }
 
 function earnPerSecond() {
-    const currentTime = new Date().getTime();
-    const elapsedTime = (currentTime - lastUpdateTime) / 1000; // Calculate elapsed time in seconds
-    lastUpdateTime = currentTime;
-
-    cash += cps * elapsedTime; // Accumulate cash over time
+    cash += cps / 10; // Update every 100 milliseconds (0.1 seconds)
     updateDisplay();
     saveGameState();
 }
@@ -47,25 +41,8 @@ function saveGameState() {
     localStorage.setItem('upgrades', upgrades);
 }
 
-function loadGameState() {
-    dbPromise.then(db => {
-        const tx = db.transaction('gameState', 'readonly');
-        const store = tx.objectStore('gameState');
-        return store.get(1);
-    }).then(gameState => {
-        if (gameState) {
-            cash = gameState.cash;
-            clickValue = gameState.clickValue;
-            cps = gameState.cps;
-            upgrades = gameState.upgrades;
-            upgradeCost = gameState.upgradeCost;
-            updateDisplay();
-        }
-    });
-}
-
-// Load the game state on page load
-document.addEventListener('DOMContentLoaded', loadGameState);
-
 // Automatically earn cash per second
-setInterval(earnPerSecond, 1000);
+setInterval(earnPerSecond, 100); // Changed from 1000 to 100 for smoother updates
+
+// Save the game state every 5 seconds
+setInterval(saveGameState, 5000);
